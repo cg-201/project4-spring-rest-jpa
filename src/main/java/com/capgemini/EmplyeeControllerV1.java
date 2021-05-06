1b89c666cc6939c52c2fe15242e78d3a8e3e1100
Santosh Mondal <santosh.ece06@gmail.com>
2021-05-06 16:40:37 +0530

    http.

--- src/main/java/com/capgemini/EmplyeeControllerV1.java
+++ src/main/java/com/capgemini/EmplyeeControllerV1.java
@@ -1,8 +1,14 @@
 package com.capgemini;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capgemini.entities.Employee;
@@ -11,31 +17,72 @@ import com.capgemini.repository.EmployeeRepository;
 @RestController
 @RequestMapping("/employee/v1/")
 public class EmplyeeControllerV1 {
-	
+
 	@Autowired
 	private EmployeeRepository employeeRepository;
 
 	@GetMapping("/create")
 	public String create(String name, String email, String mobile) {
-		
+
 		Employee e = new Employee();
 		e.setName(name);
 		e.setEmail(email);
 		e.setMobile(mobile);
-		
+
 		// adding record
 		employeeRepository.save(e);
-		
-		
+
 		return "Employee V1 Created";
 	}
 	
-	
-	public String update() {
-		return "Employee v1 Updated!!";
+	@GetMapping("/create1")
+	public String create1(@RequestParam String name, @RequestParam String email, @RequestParam String mobile) {
+
+		Employee e = new Employee();
+		e.setName(name);
+		e.setEmail(email);
+		e.setMobile(mobile);
+
+		// adding record
+		employeeRepository.save(e);
+
+		return "Employee V1 Created1";
 	}
 	
 	
+	@GetMapping("/create2")
+	public String create2(Employee e) {
+
+		// adding record
+		employeeRepository.save(e);
+
+		return "Employee V1 Created2";
+	}
+	
+	
+	@GetMapping("/find/{id}")
+	public Employee findById(@PathVariable int id) {
+		
+		Employee e = employeeRepository.findById(id).get();
+		System.out.println(e);
+		return e;
+	}
+	
+
+	@GetMapping("/update/{id}")
+	public String update(@PathVariable int id, Employee inputData) {
+		
+		Employee dbEmployee =  employeeRepository.findById(id).get();
+		if(dbEmployee != null) {
+			dbEmployee.setEmail(inputData.getEmail());
+			dbEmployee.setName(inputData.getName());
+			
+			employeeRepository.save(dbEmployee);
+		}
+		
+		return "Employee v1 Updated!!";
+	}
+
 	public String delete() {
 		return "Employee v1 delete!!";
 	}
